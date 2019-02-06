26f8061c80b9e63512e9fae90a702eeec2fef89f
Harrison Meinert-Rita <hmeinertrita@gmail.com>
2019-02-06 17:53:06 -0500

    prep for refreshing knowledge base

--- adalin.js
+++ adalin.js
@@ -1,34 +1,28 @@
-var fetch = require("node-fetch");
-const agentId = 'ada-lin';
+const dialogflow = require('dialogflow').v2beta1;
+const knowledgebase = require('./knowledgebase');
+const agent = {
+  projectId: 'ada-lin',
+  agentId: 'ada-lin',
+  knowledgeBaseDisplayName: 'wiki'
+};
+const sessionClient = new dialogflow.SessionsClient({ keyFilename: "credentials.json" });
 
 const discordIntegration = require('./discord-integration');
 const fs = require('fs');
 
 function init() {
-  process.env.GOOGLE_APPLICATION_CREDENTIALS = 'credentials.json';
   const tokens = JSON.parse(fs.readFileSync('tokens.json'));
-
-  discordIntegration.init('ada-lin', 'en-US', tokens.DISCORD_TOKEN);
+  //discordIntegration.init(sessionClient, 'ada-lin', 'en-US', tokens.DISCORD_TOKEN);
 }
+
+async function refreshKnowledgeBase(html) {
+  const encodedHtml = Buffer.from(html).toString('base64');
+  const name = knowledgebase.findKnowledgeBase(agent.projectId, agent.knowledgeBaseDisplayName);
+
+  await knowledgebase.deleteDocument(agent.projectId, name);
+  knowledgebase.createDocument(agent.projectId, agent.knowledgeBaseDisplayName, name, encodedHtml);
+}
+
 module.exports = {
   init: init
 }
-
-// const dialogflow = require('dialogflow').v2beta1;
-// const sessionClient = new dialogflow.SessionsClient();
-//
-// sessionClient.detectIntent({
-//   session: sessionClient.sessionPath(agentId, 'authTest'),
-//   queryInput: {
-//     text: {
-//       text: 'hello',
-//       languageCode: 'en-US',
-//     },
-//   },
-// })
-// .then(responses => {
-//   console.log(responses);
-// })
-// .catch(err => {
-//   console.error('ERROR:', err);
-// });
---
+++ knowledgebase.js
@@ -0,0 +1,73 @@
+const dialogflow = require('dialogflow').v2beta1;
+
+function findKnowledgeBase(projectid, displayName) {
+  resources = await listKnowledgeBases(projectId);
+  resources.forEach(r => {
+    if (r.displayName === displayName) {
+      return r;
+    }
+  });
+}
+
+async function listKnowledgeBases(projectId) {
+  // Instantiate a DialogFlow KnowledgeBasesClient.
+  const client = new dialogflow.KnowledgeBasesClient({
+    projectPath: projectId,
+  });
+
+  const formattedParent = client.projectPath(projectId);
+
+  const [resources] = await client.listKnowledgeBases({
+    parent: formattedParent,
+  });
+  return resources;
+}
+
+async function listDocuments(projectId, knowledgeBaseFullName) {
+  // Instantiate a DialogFlow Documents client.
+  const client = new dialogflow.DocumentsClient({
+    projectId: projectId,
+  });
+
+  const [resources] = await client.listDocuments({
+    parent: knowledgeBaseFullName,
+  });
+  console.log(
+    `There are ${resources.length} documents in ${knowledgeBaseFullName}`
+  );
+  return resources;
+}
+
+async function deleteDocument(projectId, documentId) {
+  // Instantiate a DialogFlow Documents client.
+  const client = new dialogflow.DocumentsClient({
+    projectId: projectId,
+  });
+
+  const [operation] = await client.deleteDocument({name: documentId});
+  const responses = await operation.promise();
+  if (responses[2].done === true) console.log(`document deleted`);
+}
+
+async function createDocument(projectId, documentName, knowledgeBaseFullName, rawContent) {
+  // Instantiate a DialogFlow Documents client.
+  const client = new dialogflow.DocumentsClient({
+    projectId: projectId,
+  });
+
+  const request = {
+    parent: knowledgeBaseFullName,
+    document: {
+      knowledgeTypes: ['EXTRACTIVE_QA'],
+      displayName: documentName,
+      rawContent: rawContent,
+      source: 'rawContent',
+      mimeType: 'text/html',
+    },
+  };
+  console.log(request.document.rawContent);
+
+  const [operation] = await client.createDocument(request);
+  const [response] = await operation.promise();
+  console.log('document created');
+}
