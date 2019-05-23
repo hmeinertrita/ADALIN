c053e6d362b6b1eb3b8cbd9992ad373918fa97ab
Harrison Meinert-Rita <harrisonmeinert-rita@harrisonmeinert-rita-MBP.local>
2019-05-23 14:40:41 -0400

    add options + empty wiki

--- server.js
+++ server.js
@@ -1,3 +1,10 @@
 const adalin = require('./adalin/module');
+const wiki = require('./wiki/module');
+const fs = require('fs')
 
+const options = await fs.promises.readFile('options.json');
+const tokens = await fs.promises.readFile('tokens.json');
+const credentials = await fs.promises.readFile('credentials.json');
+
+await wiki(options, tokens.discordToken);
 adalin(options);
