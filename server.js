1cf35802524e776d1790a07f949c5ca68d70f549
Harrison Meinert-Rita <harrisonmeinert-rita@harrisonmeinert-rita-MBP.local>
2019-05-24 14:07:03 -0400

    update packages and fix document deletion/creation

--- server.js
+++ server.js
@@ -4,11 +4,10 @@ const fs = require('fs')
 
 async function start() {
   const options = JSON.parse(await fs.promises.readFile('options.json'))
-  //const tokens = JSON.parse(await fs.promises.readFile('tokens.json'))
-  //const credentials = JSON.parse(await fs.promises.readFile('credentials.json'))
+  const tokens = JSON.parse(await fs.promises.readFile('tokens.json'))
 
-  await wiki(options)
-  //adalin(options, tokens.discordToken)
+  //await wiki(options)
+  adalin(options)//, tokens.discordToken)
 }
 
 start()
