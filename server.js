bf703595c2c68d5f29739a267361b8c62058bfb2
Harrison Meinert-Rita <harrisonmeinert-rita@harrisonmeinert-rita-MBP.local>
2019-05-24 16:02:32 -0400

    add eleusian wiki

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const adalin = require('./adalin/module')
-const wiki = require('./wiki/module')
+const wiki = require('./tiddlywiki/module')
 const fs = require('fs')
 
 async function start() {
   const options = JSON.parse(await fs.promises.readFile('options.json'))
   const tokens = JSON.parse(await fs.promises.readFile('tokens.json'))
 
-  //await wiki(options)
+  await wiki(options)
   adalin(options, tokens.discordToken)
 }
 
